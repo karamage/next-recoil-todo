7163077413de6848c2484bf23f09647c37d49cee
karamage <karamage.umauma@gmail.com>
2020-12-07 17:53:17 +0900

    tweak

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
+
+/*
 import Head from 'next/head'
 import styles from '../../styles/Home.module.css'
-/*
 import CountDisplay from '../components/CountDisplay'
 import IncrementButton from '../components/IncrementButton'
 import DecrementButton from '../components/DecrementButton'
 */
 
-export default function Home() {
+export default function Home(): JSX.Element {
   return (
     <div>
       <div>Todo App</div>
